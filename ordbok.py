b54b3cba6181224dde6537fadb7ca478ba44e7e1
mikailtirit <mikail.tirit@hotmail.com>
2025-09-17 14:42:24 +0200

    test programmet

--- ordbok.py
+++ ordbok.py
@@ -2,33 +2,7 @@
 
 
 
+# Oppgave 6 – Test programmet
 
-
-
-# Oppgave 5 – Lag en hovedmeny
-
-def hovedmeny():
-    while True:  # kjør helt til brukeren velger å avslutte
-        print("Hva vil du gjøre?")
-        print("1. Vis alle")
-        print("2. Legg til ny")
-        print("3. Søk")
-        print("4. Avslutt")
-
-        valg = input("Skriv inn ditt valg (1-4): ")
-
-        if valg == "1":
-            vis_alle()
-        elif valg == "2":
-            legg_til()
-        elif valg == "3":
-            sok()
-        elif valg == "4":
-            print("Avslutter programmet...")
-            break  # avslutter while-løkken
-        else:
-            print("Ugyldig valg, prøv igjen.\n")
-
-
-
-
+if __name__ == "__main__":
+    hovedmeny()

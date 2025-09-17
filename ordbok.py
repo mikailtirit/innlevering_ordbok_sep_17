c7f07057c32a497b12d71a7ebf532017863267b1
mikailtirit <mikail.tirit@hotmail.com>
2025-09-17 14:37:56 +0200

    lage en liste med ordbøker

---
+++ ordbok.py
@@ -0,0 +1,17 @@
+
+# oppgave 1 - lage en liste med ordbøker
+
+telefonbok = []   #lager en tom liste for kontakter
+
+
+#lage to ordbøker med personer
+pers1 = {"navn": "Mikail Tirit", "nummer": "984 92 203"}
+pers2 = {"navn": "Anders Mikaelsen", "nummer": "900 123 454"}
+
+
+
+#legger personene i lista med .append()
+telefonbok.append(pers1)
+telefonbok.append(pers2)
+
+
